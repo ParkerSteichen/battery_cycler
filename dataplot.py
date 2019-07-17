c3c1816342c09c67efd4e240d246b6dfb7923e82
Parker Steichen <parker7s@uw.edu>
2019-07-17 10:24:13 -0700

    initial copy over from notebooks

---
+++ dataplot.py
@@ -0,0 +1,60 @@
+"""
+A set a functions for plotting battery cycle performance data.
+"""
+
+def cv_cycle_plot(ax, clean_dataframe, cycle_list, cycle_num, plot_color, plot_alpha, label=None):
+    """
+    docstring
+    """
+    # fetch indeces for specified cycle_num
+    # check to see if complete cycle
+    if len(cycle_list[cycle_num-1]) == 1:
+        # check to see if initial discharge
+        if cycle_num == 1:
+            print('First cycle only contains discharge data')
+            discharge_indeces = cycle_list[cycle_num-1][0]
+            discharge_dataframe = clean_dataframe[clean_dataframe.index.isin(discharge_indeces)]
+            ax.plot(discharge_dataframe['capacity'],
+                    discharge_dataframe['voltage'],
+                    color=plot_color,
+                    alpha=plot_alpha)
+        else:
+            print('Incomplete cycle, only contains charge data')
+            charge_indeces = cycle_list[cycle_num-1][0]
+            charge_dataframe = clean_dataframe[clean_dataframe.index.isin(charge_indeces)]
+            ax.plot(charge_dataframe['capacity'],
+                    charge_dataframe['voltage'],
+                    color=plot_color,
+                    alpha=plot_alpha)
+    else:
+        # complete cycle so plot both
+        charge_indeces = cycle_list[cycle_num-1][0]
+        charge_dataframe = clean_dataframe[clean_dataframe.index.isin(charge_indeces)]
+        discharge_indeces = cycle_list[cycle_num-1][1]
+        discharge_dataframe = clean_dataframe[clean_dataframe.index.isin(discharge_indeces)]
+        # now plot charge and discharge curves
+        ax.plot(charge_dataframe['capacity'],
+                charge_dataframe['voltage'],
+                color=plot_color,
+                alpha=plot_alpha)
+        ax.plot(discharge_dataframe['capacity'],
+                discharge_dataframe['voltage'],
+                color=plot_color,
+                alpha=plot_alpha)
+
+
+def cv_plot(ax, clean_dataframe, cycle_list, input_list, plot_color):
+    """
+    docstring
+    """
+    # generate list of alpha values for fade effect
+    if len(input_list) == 1:
+        alpha_list = [1]
+    elif len(input_list) == 2:
+        alpha_list = [0.5, 1]
+    else:
+        alpha_list = np.linspace(0.25, 1, len(input_list))
+    # iterate across input_list and plot each
+    for i, cycle_num in enumerate(input_list):
+        cv_cycle_plot(ax, clean_dataframe, cycle_list, cycle_num, plot_color,
+                      alpha_list[i])
---
+++ dataprep.py
@@ -0,0 +1,114 @@
+"""
+A set of functions that prepare data from a .xlsx file for plotting
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def import_data(filename, sheet_count):
+    """
+    function to import and concatenate excel data
+    requires that exported data contains columns:
+        index, test_time, step_time, voltage, current, capacity, scapacity, energy, senergy, and state
+    """
+    names = ['index', 'test_time', 'step_time', 'voltage', 'current', 'capacity', 'scapacity', 'energy', 'senergy', 'state']
+    df = pd.read_excel(filename, sheet_name='Sheet1', header=None, names=names)
+    dfl = list([df])
+    if sheet_count == 1:
+        pass
+    else:
+        for i in range(1, sheet_count):
+            sheet_name='Sheet1(Continued{})'.format(i)
+            df0 = pd.read_excel(filename, sheet_name, header=None, names=names)
+            dfl.append(df0)
+    dataframe = pd.concat(dfl, ignore_index=True)
+    return dataframe
+
+
+def clean_prep_break(dataframe):
+    """
+    function that removes labels from in between cycles,
+    adds a new column which contains the time in seconds,
+    and outputs a list of cycle_breaks
+    """
+    times = dataframe['test_time']
+    drop_labels = ['Mode', 'Rest', 'Charge CC', 'Charge C-Rate',
+                   'Discharge CC', 'Discharge C-Rate', 'TestTime']
+    drop_index = [i for i, time_0 in enumerate(times) if time_0 in drop_labels]
+    index = [i for i, time_0 in enumerate(times) if time_0 not in drop_labels] 
+    dataframe2 = dataframe.drop(drop_index)
+    # add column with time converted to seconds
+    t_sec = []
+    times = dataframe2['test_time']
+    for i in range(len(dataframe2['test_time'])):
+        j = index[i]
+        time_0 = str(times[j])
+        if len(time_0) < 10:
+            days = 0
+            hours, minutes, seconds = time_0.split(':')
+        else:
+            days, time = time_0.split('-')
+            hours, minutes, seconds = time.split(':')
+        sec = int(days)*86400 + int(hours)*3600 + int(minutes)*60 + int(seconds)
+        t_sec.append(sec)
+    dataframe2['test_time_sec'] = t_sec
+    # converts drop_index to a list of cycle_breaks
+    cycle_break = []
+    for i in range(len(dataframe.index)):
+        if i - 1 in drop_index and i + 1 in drop_index:
+            cycle_break.append(i)
+        else:
+            pass
+    return dataframe2, cycle_break
+
+
+def return_cycle_list(clean_dataframe, cycle_break, rest=True, reverse=True):
+    # first break up the indexes for each cycle
+    # empty list to collect lists of step_indeces
+    index_list = []
+    for i in range(0, len(cycle_break)-1):
+        step_indeces = np.arange(cycle_break[i]+2, cycle_break[i+1]-1)
+        index_list.append(step_indeces)
+    # add all values after the last cycle_break as the final cycle
+    step_indeces = np.arange(cycle_break[-1], clean_dataframe.index[-1]+1)
+    index_list.append(step_indeces)
+    index_list
+    # drop rest data if True
+    if rest is True:
+        index_list.pop(0)
+        print('Rest step removed')
+    else:
+        pass
+    # if reverse is True the first cycle will only have a discharge while all
+    # others will have a charge followed by a discharge
+    cycle_list = []
+    if reverse is True:
+        cycle_list.append([index_list[0]])
+        # remove first discharge from index_list
+        index_list.pop(0)
+        print('Reverse Mode: First cycle contains only initial discharge')
+        print('{} complete cycles after intial discharge'.format(len(index_list)//2))
+    else:
+        print('{} complete cycles'.format(len(index_list)//2))
+    for i in range(0, len(index_list)-1, 2):
+        cycle = [index_list[i], index_list[i+1]]
+        cycle_list.append(cycle)
+    # if true there are an odd number of index lists indicating a partial cycle
+    if len(index_list) % 2 == 1:
+        cycle_list.append([index_list[-1]])
+        print('Final cycle only contains charge data')
+    else:
+        pass
+    return cycle_list
+
+
+def plotprep(filename, sheet_count, rest=True, reverse=True):
+    """
+    docstring
+    """
+    dataframe = import_data(filename, sheet_count)
+    clean_dataframe, cycle_break = clean_prep_break(dataframe)
+    cycle_list = return_cycle_list(clean_dataframe, cycle_break, rest, reverse)
+    return clean_dataframe, cycle_list
